31c7d534c98dd421aad66df6c67c937ab7e77510
Ilya Kardailsky <ilya.kardailsky@gmail.com>
2014-03-16 04:05:56 +1100

    use query() for basic index lookup

--- lib/pacer-titan/graph.rb
+++ lib/pacer-titan/graph.rb
@@ -1,28 +1,35 @@
 module Pacer
   module Titan
-    # hack to make indexing work:
-    class FeatureProxy
-      def initialize original_features 
-        @features = original_features 
-      end
-      
-      def supportsIndices
-        true
-      end
-      
-      def method_missing(name, *args, &block)
-        @features.public_send(name, *args, &block) 
-      end
-    end
-    
     class Graph < PacerGraph
-      def features
-        FeatureProxy.new(blueprints_graph.features)
-      end 
       
-      def indices
-        key_indices
+      # Use GraphQuery for index lookups, based on pacer-neo4j's code:
+      private
+      def indexed_route(element_type, filters, block)
+        return super if search_manual_indices
+        
+        filters.graph = self
+        filters.use_lookup!
+        query = indexed_properties(element_type, filters)
+        
+        if query
+          route = exact_query(query, element_type: element_type, extensions: filters.extensions, wrapper: filters.wrapper)
+          
+          filters.remove_property_keys key_indices(element_type)
+          if filters.any?
+            Pacer::Route.property_filter(route, filters, block)
+          else
+            route
+          end
+        elsif filters.route_modules.any?
+          mod = filters.route_modules.shift
+          Pacer::Route.property_filter(mod.route(self), filters, block)
+        end
       end
+            
+      def indexed_properties(type, filters)
+        filters.properties.select { |k, v| key_indices(type).include?(k) and not v.nil? }
+      end
+      
     end
   end
 end
--- lib/pacer-titan/titan_query.rb
+++ lib/pacer-titan/titan_query.rb
@@ -1,10 +1,23 @@
 module Pacer
   module Titan
     class Graph
-      # Use Titan's QueryBuilder to access indexes (replaces g.v(key: value) style index querying
-      # eg: g.query{ has('login', 'ilya').has('description', Text::CONTAINS, 'abc') }.out(:messages)
-      # be sure to java_import com.thinkaurelius.titan.core.attribute.Text for the above example
-      def query(options = { element_type: :vertex }, &query)
+      # Use Titan's QueryBuilder to access indices when you need to specify other comparison predicates
+      # eg: g.query{ has('description', Text::CONTAINS, 'abc').has('login', 'ilya') }.out(:messages)...
+      # be sure to import com.thinkaurelius.titan.core.attribute.Text for the above example
+      def query(options = {}, &query)
+        options[:element_type] ||= :vertex
+        
+        chain_route options.merge(
+          query: query,
+          filter: :titan_query,
+          back: self
+        )
+      end
+      
+      # Pass a hash of properties and values to look for in Titan's standard (exact) index
+      def exact_query(query, options={})
+        options[:element_type] ||= :vertex
+        
         chain_route options.merge(
           query: query,
           filter: :titan_query,
@@ -29,14 +42,34 @@ module Pacer
         
       protected
       
+      def build_graph_centric_vertex_query(properties = {})
+        path = graph.blueprints_graph.query
+        properties.each do |key, value|
+          path = path.has("#{ key }", graph.encode_property(value))
+        end
+        path
+      end
+      
       def query_result
-        path = graph.blueprints_graph.query.instance_exec(&query)
+        if query.is_a?(Array) || query.is_a?(Hash)
+          path = build_graph_centric_vertex_query(query)
+        elsif query.kind_of? Proc
+          path = graph.blueprints_graph.query.instance_exec(&query)
+        else
+          raise "Invalid query type for index lookup (takes a hash or block): #{ query }"
+        end
+        
         path.limit(top) if top
-        path = path.vertices
+        
+        if element_type == :edge
+          path.edges
+        else
+          path.vertices
+        end
       end
       
       def source_iterator
-        query_result.to_route(element_type: :vertex, graph: graph)
+        query_result.to_route(element_type: element_type, graph: graph)
       end
       
       def inspect_string
