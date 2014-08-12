19b95d6604ebd9a5be9222ffdbbfec7c253a6483
Ilya Kardailsky <ilya.kardailsky@gmail.com>
2014-08-12 16:25:50 +1000

    bump titan version to 0.5.0-M3

--- lib/pacer-titan/version.rb
+++ lib/pacer-titan/version.rb
@@ -1,11 +1,11 @@
 module Pacer
   module Titan
-    VERSION = "0.0.3"
+    VERSION = "0.0.4"
     JAR = "pacer-titan-#{ VERSION }-standalone.jar"
     JAR_PATH = "lib/#{ JAR }"
-    BLUEPRINTS_VERSION = "2.4.0"
-    PIPES_VERSION = "2.4.0"
-    TITAN_VERSION = "0.4.2"
+    BLUEPRINTS_VERSION = "2.5.0"
+    PIPES_VERSION = "2.5.0"
+    TITAN_VERSION = "0.5.0-M3"
     PACER_REQ = ">= 1.4.0"
   end
 end
